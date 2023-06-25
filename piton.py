f423d3d8fa40ae4362a3e91513563b5f81752a89
Екатерина <130562550+EkaterinaKhromova@users.noreply.github.com>
2023-06-25 13:27:14 +0300

    Add files via upload

---
+++ piton.py
@@ -0,0 +1,22 @@
+print('privet')
+# 3, 0, '',[], False, True, 'mama'
+if 7:
+    print(7, 'истинно')
+if 3:
+    print(3, 'истинно')
+if '':
+    print('', 'истинно')
+if []:
+    print([], 'истинно')
+if False:
+    print(False, 'истинно')
+if  True:    
+    print(True, 'истинно')
+if 'mama':    
+    print('mama', 'истинно')
+if -7:
+    print(-7, 'истинно')
+if (0,6):
+    print(0,6, 'истинно')
+if 0.6:
+    print(0.6, 'истинно')
