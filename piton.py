bef1c745d05f36b4b3834fc70319503a61f2197c
Ekaterina <KaterinaCV284@yandex.ru>
2023-06-25 16:31:40 +0300

    Написали условия посещения трасс

--- piton.py
+++ piton.py
@@ -1,22 +1,23 @@
-print('privet')
-# 3, 0, '',[], False, True, 'mama'
-if 7:
-    print(7, 'истинно')
-if 3:
-    print(3, 'истинно')
-if '':
-    print('', 'истинно')
-if []:
-    print([], 'истинно')
-if False:
-    print(False, 'истинно')
-if  True:    
-    print(True, 'истинно')
-if 'mama':    
-    print('mama', 'истинно')
-if -7:
-    print(-7, 'истинно')
-if (0,6):
-    print(0,6, 'истинно')
-if 0.6:
-    print(0.6, 'истинно')
+a = int(input('Введите рост '))
+
+# print('privet')
+# if 7:
+#     print(7, 'истинно')
+# if 3:
+#     print(3, 'истинно')
+# if '':
+#     print('', 'истинно')
+# if []:
+#     print([], 'истинно')
+# if False:
+#     print(False, 'истинно')
+# if  True:    
+#     print(True, 'истинно')
+# if 'mama':    
+#     print('mama', 'истинно')
+# if -7:
+#     print(-7, 'истинно')
+# if (0,6):
+#     print(0,6, 'истинно')
+# if 0.6:
+#     print(0.6, 'истинно')
